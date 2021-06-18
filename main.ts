17dfb26e6179b97235959ed10f5251610f0ed5ff
aarnavvj <81868372+aarnavvj@users.noreply.github.com>
2021-06-18 18:54:21 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onButtonPressed(Button.A, function () {
+    // This code gets temperature values and stores the values using the "serial" block.
     for (let index = 0; index <= 1000; index++) {
         serial.writeValue("Temperature", input.temperature())
         basic.pause(2000)
